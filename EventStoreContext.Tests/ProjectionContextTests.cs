9d114e547a3bd953c7fbf5db01ab544e79995c43
Arsen <timchuk201092@gmail.com>
2017-12-15 13:05:57 +0200

    Create EventStore tests

---
+++ EventStoreContext.Tests/ProjectionContextTests.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using EventStore.ClientAPI;
+using EventStore.ClientAPI.Embedded;
+using EventStore.ClientAPI.Projections;
+using EventStore.ClientAPI.SystemData;
+using EventStore.Common.Options;
+using EventStore.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EventStore.ClientAPI.Common.Log;
+using EventStore.Projections.Core.Services.Management;
+
+namespace EventStoreContext.Tests
+{
+    [TestClass]
+    public class ProjectionContextTests
+    {
+        private ClusterVNode node;
+        private readonly IPEndPoint tcp = new IPEndPoint(IPAddress.Loopback, 1113);
+        private readonly IPEndPoint http = new IPEndPoint(IPAddress.Loopback, 2113);
+        private readonly UserCredentials credentials = new UserCredentials("admin", "changeit");
+
+        private ClusterVNode InitNode()
+        {
+            return EmbeddedVNodeBuilder.AsSingleNode()
+                .RunInMemory()
+                .WithExternalTcpOn(tcp)
+                .WithExternalHttpOn(http)
+                .RunProjections(ProjectionType.All)
+                .StartStandardProjections()
+                .Build();
+        }
+
+        private async Task<ProjectionsManager> InitProjectionManager()
+        {
+            node = InitNode();
+            node = await node.StartAndWaitUntilReady();
+
+            return new ProjectionsManager(new ConsoleLogger(), http, new TimeSpan(1, 0, 0));
+        }
+
+        [TestMethod]
+        public async Task TestSetupNode()
+        {
+            try
+            {
+                node = InitNode();
+                node = await node.StartAndWaitUntilReady();
+
+                Assert.IsNotNull(node);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        [TestMethod]
+        public async Task ConnectionTest()
+        {
+            node = InitNode();
+            node = await node.StartAndWaitUntilReady();
+
+            var connection = EmbeddedEventStoreConnection.Create(node,
+                ConnectionSettings.Create()
+                    .EnableVerboseLogging()
+                    .UseConsoleLogger()
+                    .SetDefaultUserCredentials(credentials)
+                    .Build());
+
+            await connection.ConnectAsync();
+            
+            connection.Connected += (sender, args) =>
+            {
+                connection = args.Connection;
+            };
+
+            Assert.IsNotNull(connection);
+        }
+
+        
+
+        [TestMethod]
+        public async Task GetAllProjectionsTest()
+        {
+            var projectionManager = await InitProjectionManager();
+
+            var list = await projectionManager.ListAllAsync(credentials);
+
+            Assert.IsNotNull(list);
+        }
+
+        [TestMethod]
+        public async Task CreateProjectionTest()
+        {
+            var projectionManager = await InitProjectionManager();
+
+            //string name = ""
+
+            //projectionManager.CreateContinuousAsync()
+        }
+    }
+}
---
+++ EventStoreContext.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EventStoreContext.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EventStoreContext.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("25976d26-a5c2-41f1-b12e-7a5e1e07186d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
