95c1842ae9c40e581fdcb8e321cc5fb3e43f29f4
Arsen <timchuk201092@gmail.com>
2017-12-15 13:04:16 +0200

    Add another contracts

---
+++ AnotherClient.Contracts/Commands/CreateOrderCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AnotherClient.Contracts.Commands
+{
+    public class CreateOrderCommand
+    {
+        public Guid Id { get; set; }
+        public Guid CustomerId { get; set; }
+        public string BillingAddress { get; set; }
+
+    }
+}
