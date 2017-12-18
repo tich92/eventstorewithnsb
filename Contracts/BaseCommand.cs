c0730daff1c6981feb50ebbe3eb6e5fb843c1894
Arsen <timchuk201092@gmail.com>
2017-12-18 11:42:18 +0200

    Base contracts

---
+++ Contracts/BaseCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Contracts
+{
+    /// <summary>
+    /// Base command class
+    /// </summary>
+    public abstract class BaseCommand
+    {
+        public DateTime CreatedDate { get; set; }
+    }
+}
