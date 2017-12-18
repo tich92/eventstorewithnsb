aa918820f92673d89087c6f65c28ccabc283fb69
Arsen <timchuk201092@gmail.com>
2017-12-18 18:13:02 +0200

    Fix init mock db context

--- OrderProcessor.Tests/OrderHandlerTests.cs
+++ OrderProcessor.Tests/OrderHandlerTests.cs
@@ -28,32 +28,13 @@ namespace OrderProcessor.Tests
 
             var mockOrderDbSet = new MockedDbContext<OrderContext>();
 
+            mockOrderDbSet.MockTables();
+
             var mappingConfig = new MappingConfig();
             
             orderHandler = new OrderHandler(mappingConfig.Mapper, mockOrderDbSet.Object);
         }
-
-        public static DbSet<T> MockDbSet<T>(List<T> table) where T : class
-        {
-            var dbSet = new Mock<DbSet<T>>();
-            dbSet.As<IQueryable<T>>().Setup(q => q.Provider).Returns(() => table.AsQueryable().Provider);
-            dbSet.As<IQueryable<T>>().Setup(q => q.Expression).Returns(() => table.AsQueryable().Expression);
-            dbSet.As<IQueryable<T>>().Setup(q => q.ElementType).Returns(() => table.AsQueryable().ElementType);
-            dbSet.As<IQueryable<T>>().Setup(q => q.GetEnumerator()).Returns(() => table.AsQueryable().GetEnumerator());
-            dbSet.Setup(set => set.Add(It.IsAny<T>())).Callback<T>(table.Add);
-            dbSet.Setup(set => set.AddRange(It.IsAny<IEnumerable<T>>())).Callback<IEnumerable<T>>(table.AddRange);
-            dbSet.Setup(set => set.Remove(It.IsAny<T>())).Callback<T>(t => table.Remove(t));
-            dbSet.Setup(set => set.RemoveRange(It.IsAny<IEnumerable<T>>())).Callback<IEnumerable<T>>(ts =>
-            {
-                foreach (var t in ts)
-                {
-                    table.Remove(t);
-                }
-            });
-
-            return dbSet.Object;
-        }
-
+        
         private async Task<IEnumerable<object>> GetEvents()
         {
             return await eventContext.ReadStreamEventsForward(StreamName);
