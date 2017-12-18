acf341b1666d9de6599ba733f53eb29232d0cd99
Arsen <timchuk201092@gmail.com>
2017-12-18 14:27:41 +0200

    Add event store test with getting type fron external assemblies

--- EventStoreContext.Tests/EventStoreContextTest.cs
+++ EventStoreContext.Tests/EventStoreContextTest.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -7,24 +6,20 @@ using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Newtonsoft.Json;
-using TypeScanner;
-
 
 namespace EventStoreContext.Tests
 {
     [TestClass]
     public class EventStoreContextTest
     {
-        private readonly EventContext eventContext;
-        private readonly ITypeScanner typeScanner;
-
+        private EventContext eventContext;
+        
         public EventStoreContextTest()
         {
-            eventContext = new EventContext();
-            typeScanner = new TypeScanner.TypeScanner();
+            eventContext = new EventContext();   
         }
 
-        private IEnumerable<Assembly> TestGetAssemblyWithTypes()
+        private IEnumerable<Assembly> GetReferencedTypes()
         {
             var loadedAssemblies = AppDomain.CurrentDomain.GetAssemblies().ToList();
             var loadedPaths = loadedAssemblies.Select(a => a.Location).ToArray();
@@ -32,34 +27,33 @@ namespace EventStoreContext.Tests
             var referencedPaths = Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory, "*.dll");
             var toLoad = referencedPaths.Where(r => !loadedPaths.Contains(r, StringComparer.InvariantCultureIgnoreCase)).ToList();
 
-            loadedAssemblies.AddRange(toLoad.Select(path => AppDomain.CurrentDomain.Load(AssemblyName.GetAssemblyName(path))));
+            foreach (var path in toLoad)
+            {
+                loadedAssemblies.Add(AppDomain.CurrentDomain.Load(AssemblyName.GetAssemblyName(path)));
+            }
 
             return loadedAssemblies;
         }
 
-        private Type GetDerrivedType(string typeName)
+        private Type GetTypeByName(string name)
         {
-            return TestGetAssemblyWithTypes().SelectMany(o => o.GetTypes()).FirstOrDefault(o => o.FullName == typeName);
+            var references = GetReferencedTypes();
 
-            //return Assembly
-            //    .GetExecutingAssembly()
-            //    .GetReferencedAssemblies()
-            //    .Select(Assembly.Load)
-            //    .SelectMany(x => x.GetTypes()).FirstOrDefault(x => x.FullName == typeName);
+            return references.SelectMany(t => t.GetTypes()).FirstOrDefault(t => t.FullName == name);
         }
 
         [TestMethod]
-        public void GetTypeFromReferencedAssemblyTest()
+        public void GetAssemblyTypeTest()
         {
             var fullName = "Contracts.Events.CreatedOrderItemEvent";
-            
-            var type = GetDerrivedType(fullName);
-            
+
+            var type = GetTypeByName(fullName);
+
             Assert.IsNotNull(type);
         }
 
         [TestMethod]
-        public async Task ReadEventsTest()
+        public async Task ReadEventsFromStreamTestAsync()
         {
             var streamName = "Order c0c8b62c-607e-4298-824c-1a73f7361f75";
 
@@ -69,9 +63,13 @@ namespace EventStoreContext.Tests
             {
                 var eventType = @event.EventType;
 
-                var data = JsonConvert.DeserializeObject(@event.EventData, GetDerrivedType(eventType));
+                var data = JsonConvert.DeserializeObject(@event.EventData, GetTypeByName(eventType));
 
-                Assert.IsNotNull(@event);
+                Assert.IsNotNull(data);
+
+                AssemblyName assemblyName = data.GetType().Assembly.GetName();
+
+                Assert.AreEqual(assemblyName.Name, "Contracts");
             }
         }
     }
